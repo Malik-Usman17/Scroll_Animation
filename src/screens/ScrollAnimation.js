8afb782a67c57bfa2cfaebb32cbc0d0a762247a4
Malik Muhammad Usman <usmaan.maalik9503@gmail.com>
2022-08-10 18:58:21 +0500

    some ui changes in scroll animation screen

--- src/screens/ScrollAnimation.js
+++ src/screens/ScrollAnimation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView } from 'react-native';
+import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView, ImageBackground } from 'react-native';
 
 // import faker from 'faker'
 import { faker } from '@faker-js/faker';
@@ -31,72 +31,97 @@ const ScrollAnimation = () => {
   const scrollY = React.useRef(new Animated.Value(0)).current;
 
   return (
-  <View style={{ flex: 1, backgroundColor: '#fff' }}>
-    {/* <StatusBar hidden /> */}
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      {/* <StatusBar hidden /> */}
 
-    <Image 
-      source={require('../assets/bg.jpg')}
-      // resizeMode='contain'
-      style={StyleSheet.absoluteFillObject}
-      // style={{flex: 1}}
-      blurRadius={80}
-    />
+      {/* <Image
+        source={require('../assets/bg.jpg')}
+        // resizeMode='contain'
+        style={StyleSheet.absoluteFillObject}
+        // style={{flex: 1}}
+        blurRadius={80}
+      /> */}
+    <ImageBackground source={require('../assets/bg.jpg')} style={StyleSheet.absoluteFillObject} blurRadius={30}>
+      
+      <Animated.FlatList
+        data={DATA}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(item, index) => index}
+        contentContainerStyle={{ padding: SPACING, paddingTop: StatusBar.currentHeight || 42 }}
+        renderItem={({ item, index }) => {
 
-    <Animated.FlatList 
-      data={DATA}
-      onScroll={Animated.event(
-        [{nativeEvent: {contentOffset: {y: scrollY}}}],
-        {useNativeDriver: true}
-      )}
-      keyExtractor={(item, index) => index}
-      contentContainerStyle={{padding: SPACING, paddingTop: StatusBar.currentHeight || 42}}
-      renderItem={({item, index}) => {
-        
-        const inputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 2)]
-        const scale = scrollY.interpolate({
-          inputRange,
-          outputRange: [1, 1, 1, 0]
-        })
+          const inputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 2)]
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0]
+          })
 
-        const opacityInputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 1)]
-        const opacity = scrollY.interpolate({
-          inputRange: opacityInputRange,
-          outputRange: [1, 1, 1, 0]
-        })
+          const opacityInputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 1)]
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
+            outputRange: [1, 1, 1, 0]
+          })
 
 
-        return(
-          <Animated.View style={{...styles.flatListComponentContainer, transform: [{scale}], opacity: opacity}}>
-            <Image 
-              source={{uri: item.image}}
-              style={{width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE, marginRight: SPACING / 2}}
-            />
+          return (
+            <Animated.View style={{ ...styles.flatListComponentContainer, transform: [{ scale }], opacity: opacity }}>
+              <Image
+                source={{ uri: item.image }}
+                style={styles.image}
+              />
 
-            <View>
-              <Text style={{fontSize: 22, fontWeight: '700', color: 'black'}}>{item.name}</Text>
-              <Text style={{fontSize: 18, opacity: 0.7}}>{item.jobTitle}</Text>
-              <Text style={{fontSize: 14, opacity: 0.8, color: '#0099cc'}}>{item.email}</Text>
-            </View>
-          </Animated.View>
-        )
-      }}
-    />
-  </View>
+              <View style={{ flex: 1 }}>
+                <Text style={styles.personName}>{item.name}</Text>
+                <Text style={styles.jobTitle}>{item.jobTitle}</Text>
+                <Text style={styles.email}>{item.email}</Text>
+              </View>
+            </Animated.View>
+          )
+        }}
+      />
+
+      </ImageBackground>
+    
+    </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-  flatListComponentContainer:{
-    flexDirection: 'row', 
-    padding: SPACING, 
-    marginBottom: SPACING, 
-    backgroundColor: 'rgba(255, 255, 255, 0.8)', 
-    borderRadius: 12, 
-    shadowColor: '#000', 
-    shadowOffset: {width: 0, height: 10},
+  flatListComponentContainer: {
+    flexDirection: 'row',
+    padding: SPACING,
+    marginBottom: SPACING,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
     shadowOpacity: 0.3,
     shadowRadius: 20,
+  },
+  image: {
+    width: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    borderRadius: AVATAR_SIZE,
+    marginRight: SPACING / 2
+  },
+  personName:{
+    fontSize: 22, 
+    fontWeight: '700', 
+    color: 'black' 
+  },
+  jobTitle:{ 
+    fontSize: 18, 
+    opacity: 0.7, 
+    color: 'black' 
+  },
+  email:{ 
+    fontSize: 14, 
+    opacity: 0.8, 
+    color: '#0099cc' 
   }
 })
 
--- src/screens/UseEffectScreen.js
+++ src/screens/UseEffectScreen.js
@@ -1,15 +1,87 @@
 import React, {useEffect, useState} from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from "react-native";
+import { useIsFocused } from '@react-navigation/native';
+import { Axios } from "axios";
 
 
 
-const UseEffectScreen = () => {
+const UseEffectScreen = ({navigation}) => {
+
+  const [myName, setMyName] = useState('Malik Usman');
+  const [data, setData] = useState('');
+  const isFocused = useIsFocused();
+
+  // console.log('FOCUESD:',isFocused)
+
+  const geTDataFromApi = async () => {
+    console.log('running')
+    try {
+      const apiRespone = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      // console.log('API RESPONSE:',await apiRespone.json())
+      setData(await apiRespone.json())
+    } 
+    catch (error) {
+      alert(error)
+    }
+  }
+
+  const nextScreen = () => {
+    console.log('Method running!!')
+    setTimeout(() => {
+      navigation.navigate('ScrollAnimation')
+    }, 2000);
+    // navigation.navigate('ScrollAnimation')
+  }
+
+  useEffect(() => {
+    geTDataFromApi()
+  }, [])
+
   return(
-    <View>
-      <Text>helloooo</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+      <View style={styles.innerContainer}>
+
+        <TextInput
+          style={styles.inputField} 
+          value={myName}
+          onChangeText={setMyName}
+        />
+
+        <Text style={{fontSize: 18, fontWeight: 'bold', color: 'black', marginVertical: 15}}>{myName}</Text>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ScrollAnimation')}>
+          <Text style={{color: 'white', fontSize: 18, fontWeight: '600'}}>Press It!!</Text>
+        </TouchableOpacity>
+
+      </View>
+      
     </View>
   )    
 }
+
+
+const styles = StyleSheet.create({
+  innerContainer:{
+    backgroundColor: 'pink', 
+    borderRadius: 10,
+    width: '80%',
+    padding: 15
+  },
+  inputField:{
+    backgroundColor: 'white', 
+    paddingHorizontal: 12, 
+    borderRadius: 15, 
+    width: '100%'
+  },
+  button:{
+    backgroundColor: 'blue', 
+    marginTop: 20,
+    alignItems: 'center',
+    borderRadius: 10, 
+    padding: 10
+  }
+});
 
 
 export default UseEffectScreen;
