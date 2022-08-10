18e37fe83009a8103fcaef2ed8f4291ed5f5f0e1
malik.usman <malik.usman@int.sybrid.com>
2022-08-10 16:25:35 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView } from 'react-native';
+
+// import faker from 'faker'
+import { faker } from '@faker-js/faker';
+
+const { width, height } = Dimensions.get('screen');
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.random.alphaNumeric,
+    // key:  faker.random.uuid(),
+    image: faker.image.avatar(),
+    // image: `https://randomuser.me/api/portraits/${faker.helpers.fake(['women', 'men'])}/${faker.random.numeric(60)}.jpg`,
+    // image: `https://randomuser.me/api/portraits/${faker.helpers.randomize(['women', 'men'])}/${faker.random.number(60)}.jpg`,
+    //name: faker.name.findName(),
+    name: faker.name.fullName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const BG_IMG = `https://www.pexels.com/photo/pink-rose-closeup-photography-1231265/`
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
+
+
+const App = () => {
+
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  return (
+  <View style={{ flex: 1, backgroundColor: '#fff' }}>
+    {/* <StatusBar hidden /> */}
+
+    <Image 
+      source={require('./src/assets/bg.jpg')}
+      // resizeMode='contain'
+      style={StyleSheet.absoluteFillObject}
+      // style={{flex: 1}}
+      blurRadius={80}
+    />
+
+    <Animated.FlatList 
+      data={DATA}
+      onScroll={Animated.event(
+        [{nativeEvent: {contentOffset: {y: scrollY}}}],
+        {useNativeDriver: true}
+      )}
+      keyExtractor={(item, index) => index}
+      contentContainerStyle={{padding: SPACING, paddingTop: StatusBar.currentHeight || 42}}
+      renderItem={({item, index}) => {
+        
+        const inputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 2)]
+        const scale = scrollY.interpolate({
+          inputRange,
+          outputRange: [1, 1, 1, 0]
+        })
+
+        const opacityInputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 1)]
+        const opacity = scrollY.interpolate({
+          inputRange: opacityInputRange,
+          outputRange: [1, 1, 1, 0]
+        })
+
+
+        return(
+          <Animated.View style={{...styles.flatListComponentContainer, transform: [{scale}], opacity: opacity}}>
+            <Image 
+              source={{uri: item.image}}
+              style={{width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE, marginRight: SPACING / 2}}
+            />
+
+            <View>
+              <Text style={{fontSize: 22, fontWeight: '700', color: 'black'}}>{item.name}</Text>
+              <Text style={{fontSize: 18, opacity: 0.7}}>{item.jobTitle}</Text>
+              <Text style={{fontSize: 14, opacity: 0.8, color: '#0099cc'}}>{item.email}</Text>
+            </View>
+          </Animated.View>
+        )
+      }}
+    />
+  </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  flatListComponentContainer:{
+    flexDirection: 'row', 
+    padding: SPACING, 
+    marginBottom: SPACING, 
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+    borderRadius: 12, 
+    shadowColor: '#000', 
+    shadowOffset: {width: 0, height: 10},
+    shadowOpacity: 0.3,
+    shadowRadius: 20,
+  }
+})
+
+
+export default App;
