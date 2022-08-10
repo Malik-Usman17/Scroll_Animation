6c643c97d81f5faec521ae4d8cf7f28af1b726a1
malik.usman <malik.usman@int.sybrid.com>
2022-08-10 16:50:16 +0500

    adding a new screen

---
+++ src/screens/UseEffectScreen.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+import { View, Text } from "react-native";
+
+
+
+const UseEffectScreen = () => {
+  return(
+    <View>
+      <Text>helloooo</Text>
+    </View>
+  )    
+}
+
+
+export default UseEffectScreen;
